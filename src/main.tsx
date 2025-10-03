f943cd18ec42bd304c8b15103e98f84c83a032eb
Calas <barbara.calas@orange.fr>
2025-10-03 16:49:14 +0200

    Mise en place des fonctionnalités d'authentification avec les routes Login et Register

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { store } from "./app/store";
+import { setToken } from "./features/auth/authSlice";
 import App from "./App";
 import "./index.css";
+
+const token = localStorage.getItem('token');
+if (token) {
+  store.dispatch(setToken(token));
+}
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LoginPage from "../pages/LoginPage";
 import RegisterPage from "../pages/RegisterPage";
 import Dashboard from "../pages/Dashboard";
+import ProtectedRoute from "../components/ProtectedRoute";
 
 export default function AppRoutes() {
     return (
@@ -9,7 +10,9 @@ export default function AppRoutes() {
             <Routes>
                 <Route path="/login" element={<LoginPage />} />
                 <Route path="/register" element={<RegisterPage />} />
-                <Route path="/" element={<Dashboard />} />
+                <Route element={<ProtectedRoute />}>
+                    <Route path="/" element={<Dashboard />} />
+                </Route>   
             </Routes>
         </Router>
     );
